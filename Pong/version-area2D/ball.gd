aa99cb174516110e77518a8d2a6080b2d57ffcb3
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2017-08-15 16:21:32 -0400

    screen_size variable

--- Pong/version-area2D/ball.gd
+++ Pong/version-area2D/ball.gd
@@ -16,6 +16,9 @@ export(float, 1.0, 4.0, 0.1) var speed_mult = 1.1
 var direction = Vector2(1,0)
 # The current speed of the ball
 var speed
+# The size of the screen
+# Initialized on _ready()
+var screen_size
 
 # +------------------------------------------------+
 # | ENGINE FUNCTIONS                               |
@@ -32,7 +35,8 @@ func _ready():
 func reset_ball():
 	speed = initial_speed
 	direction = Vector2(1,0)
-	set_pos(get_viewport_rect().size * 0.5)
+	screen_size = get_viewport_rect().size
+	set_pos(screen_size * 0.5)
 
 # Called when the ball bounces off a paddle
 func hit_paddle():
