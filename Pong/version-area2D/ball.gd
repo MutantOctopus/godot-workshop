d28babd931637ba72260651ba6d33d3294a871f9
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2017-08-15 16:00:07 -0400

    Starting ball script _process is not yet implemented; paddles have been given a Group

---
+++ Pong/version-area2D/ball.gd
@@ -0,0 +1,50 @@
+extends Area2D
+
+# +------------------------------------------------+
+# | EXPORTED VARIABLES                             |
+# +------------------------------------------------+
+# The speed at which the ball starts
+export(int, 0, 1000) var initial_speed = 80
+# The value that 'speed' is multiplied by when
+# bouncing off a paddle
+export(float, 1.0, 4.0, 0.1) var speed_mult = 1.1
+
+# +------------------------------------------------+
+# | MEMBER VARiABLES                               |
+# +------------------------------------------------+
+# The current direction of the ball
+var direction = Vector2(1,0)
+# The current speed of the ball
+var speed
+
+# +------------------------------------------------+
+# | ENGINE FUNCTIONS                               |
+# +------------------------------------------------+
+func _ready():
+	speed = initial_speed
+	randomize()
+	set_process(true)
+
+# +------------------------------------------------+
+# | MEMBER FUNCTIONS                               |
+# +------------------------------------------------+
+# Called when the ball is reset
+func reset_ball():
+	speed = initial_speed
+	direction = Vector2(1,0)
+	set_pos(get_viewport_rect().size * 0.5)
+
+# Called when the ball bounces off a paddle
+func hit_paddle():
+	speed *= 1.1 # Increase speed
+	direction.x = -sign(direction.x)
+	direction.y = randf() * 2 - 1
+	direction = direction.normalized()
+
+# +------------------------------------------------+
+# | SIGNAL CALLBACKS                               |
+# +------------------------------------------------+
+# Callback for self.area_enter signal
+func _on_area_enter(area):
+	var groups = area.get_groups() # Array of strings
+	if groups.has("paddle"): hit_paddle()
