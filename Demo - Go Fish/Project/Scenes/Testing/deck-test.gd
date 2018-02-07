d90f6a1a042d11c04625e0cc6a1d1bb3c7c32d95
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2018-02-07 00:49:19 -0500

    Updated testing Can now test all functions of deck - add/remove cards. Error is raised during placing card: "Condition !is_inside_tree() is true".

--- Demo - Go Fish/Project/Scenes/Testing/deck-test.gd
+++ Demo - Go Fish/Project/Scenes/Testing/deck-test.gd
@@ -1,15 +1,26 @@
 extends Node
 
 var card_pos
-var deck
+var deck1
+var deck2
+var held_card = null
 
 func _ready():
-	card_pos = get_node("pos").get_pos()
-	deck = get_node("deck")
+	card_pos = get_node("hold-pos").get_pos()
+	deck1 = get_node("deck-1")
+	deck2 = get_node("deck-2")
 
 func draw_card():
-	var card = deck.draw_card()
-	if not card == null:
-		card.set_pos(card_pos)
-		add_child(card)
-		card.flip()
+	if held_card == null:
+		var card = deck1.draw_card()
+		if not card == null:
+			card.set_pos(card_pos)
+			add_child(card)
+			card.flip()
+			held_card = card
+
+func place_card():
+	if not held_card == null:
+		remove_child(held_card)
+		deck2.place_card(held_card)
+		held_card = null
